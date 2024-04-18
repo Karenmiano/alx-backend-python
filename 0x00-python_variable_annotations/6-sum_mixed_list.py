e823eb43603cd0a93d97f3f2dd2a3d1f0ca0d17f
Karenmiano <mianokaren1798@gmail.com>
2024-04-18 20:41:04 +0300

    use union to define more than one type in list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+"""Defines function sum_mixed_list"""
 from typing import List, Union
+
+
 def sum_mixed_list(mxd_list: List[Union[int, str]]) -> float:
     """Returns the sum of integers and floats in a list"""
     return sum(mxd_list)
