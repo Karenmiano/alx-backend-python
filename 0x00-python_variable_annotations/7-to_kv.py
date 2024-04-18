b16022859de18145b1cfc9e68ab1f6c0c9cf10a9
Karenmiano <mianokaren1798@gmail.com>
2024-04-18 21:02:32 +0300

    function parameter can take on two types

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,9 +2,9 @@
 """
 Defines function to_kv
 """
-from typing import Tuple
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: int | float) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple of function arguments"""
     return (k, v**2)
