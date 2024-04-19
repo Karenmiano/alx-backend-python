c2232d898f1eae4969351a0115d5179ce01b235c
Karenmiano <mianokaren1798@gmail.com>
2024-04-19 20:08:07 +0300

    using TypeVar to keep track of unknown values

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python3
+"""
+Defines a function safely_get_value
+"""
+from typing import Mapping, Any, Union, TypeVar
 
+T = TypeVar('T')
+ret = Union[Any, T]
+Def = Union[T, None]
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> ret:
+    """
+    Returns the value of a key in a dictionary
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
